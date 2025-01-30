db45707dabc3b58877049c69de89bae36c243724
sepun2004 <145572771+sepun2004@users.noreply.github.com>
2025-01-30 19:52:04 +0100

    Add files via upload

--- source/so_long.c
+++ source/so_long.c
@@ -6,7 +6,7 @@
 /*   By: sepun <sepun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:30:11 by sepun             #+#    #+#             */
-/*   Updated: 2025/01/29 21:28:51 by sepun            ###   ########.fr       */
+/*   Updated: 2025/01/30 19:50:45 by sepun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,25 +65,53 @@ void convert_texture_to_image(t_data *s_data, mlx_t *mlx)
 	s_data->texture->exit_texture = img;
 }
 
-void img_to_window(mlx_t *mlx, mlx_image_t *img)
+void img_to_window(mlx_t *mlx, mlx_image_t *img, int i, int j)
 {
-	if (!mlx_image_to_window(mlx, img, 0, 0))
+	// printf("i: %d, j: %d\n", i, j);
+	// printf("Ingreso \n");
+	if (mlx_image_to_window(mlx, img, i, j) < 0)
 		ft_printf_error("Error: No se pudo convertir la imagen a ventana.");
 	
 }
 
 void ft_texture(t_data *map, mlx_t *mlx)
 {
+	int i;
+	int j;
 	load_textures(map);
-	
 	convert_texture_to_image(map, mlx);
-	img_to_window(mlx, map->texture->wall_texture);
-	img_to_window(mlx, map->texture->backgound_texture);
-	img_to_window(mlx, map->texture->player_texture);
-	img_to_window(mlx, map->texture->collectible_texture);
-	img_to_window(mlx, map->texture->exit_texture);
+	i = 0;
+	printf("map->map_x: %d\n", map->map_x);	
+	printf("map->map_y: %d\n", map->map_y);	
+	while (i < map->map_y)
+	{
+		j = 0;
+		while (j < map->map_x)
+		{
+			printf("----i: %d, j: %d----\n", i, j);
+			printf("map->run_map[i][j]: %c\n", map->run_map[i][j]);
+			if (map->run_map[i][j] == '1')
+				img_to_window(mlx, map->texture->wall_texture, i * 64, j * 64);
+			else if (map->run_map[i][j] == '0')
+				img_to_window(mlx, map->texture->backgound_texture, i * 64, j * 64);
+			else if (map->run_map[i][j] == 'P')
+				img_to_window(mlx, map->texture->player_texture, i * 64, j * 64);
+			else if (map->run_map[i][j] == 'C')
+				img_to_window(mlx, map->texture->collectible_texture, i * 64, j * 64);
+			else if (map->run_map[i][j] == 'E')
+				img_to_window(mlx, map->texture->exit_texture, i * 64, j * 64);
+			j++;
+		}
+		i++;
+	}
+	
+	// img_to_window(mlx, map->texture->wall_texture);
+	// img_to_window(mlx, map->texture->backgound_texture);
+	// img_to_window(mlx, map->texture->player_texture);
+	// img_to_window(mlx, map->texture->collectible_texture);
+	// img_to_window(mlx, map->texture->exit_texture);
 	// render_map(); // mlx_image_to_window //Esto va en el renderizado // dibujar el player separado
-	//mlx_loop(mlx);
+	mlx_loop(mlx);
 	//Delete_texture / delete_image
 }
 
