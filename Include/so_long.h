421d6caca77f7c2f309a12136300a0be3bb8256e
sepun2004 <145572771+sepun2004@users.noreply.github.com>
2025-01-30 17:01:37 +0100

    123

--- Include/so_long.h
+++ Include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: sepun <sepun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:30:00 by sepun             #+#    #+#             */
-/*   Updated: 2024/12/16 18:22:57 by sepun            ###   ########.fr       */
+/*   Updated: 2025/01/10 19:06:58 by sepun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,30 @@
 #include "../libft/libft.h"
 #include "../MLX42/include/MLX42/MLX42.h"
 
-typedef struct s_map
+// typedef struct s_map
+// {
+// 	int i;
+// 	int j;
+// 	int k;
+// 	int map_x;
+// 	int map_y;
+// 	int coins;
+// 	char **test_map;
+// 	char **run_map;
+// }               t_map;
+
+typedef struct s_texture
 {
+	void	*wall_texture;
+	void	*player_texture;
+	void	*exit_texture;
+	void	*collectible_texture;
+	void 	*backgound_texture;
+}			t_texture;
+
+typedef struct s_data
+{
+	// mlx_t	*mlx;
 	int i;
 	int j;
 	int k;
@@ -26,26 +48,24 @@ typedef struct s_map
 	int coins;
 	char **test_map;
 	char **run_map;
-}               t_map;
-
-typedef struct s_data
-{
-	// mlx_t	*mlx;
-	t_map	*map;
 	
+	t_texture	*texture;
 }			t_data;
 
+
+
 //PARCE_MAP
-void	ft_check_rectangle_map(t_map *game);
-void	ft_check_border(t_map *game);
-void	ft_check_objects(t_map *test_map);
+void	ft_check_rectangle_map(t_data *game);
+void	ft_check_border(t_data *game);
+void	ft_check_objects(t_data *test_map);
 int		ft_check_ber(char **argv);
 
 //so_long
-void ft_check_map(t_map *map, char **argv);
-void parce_map(t_map *map);
+void ft_check_map(t_data *map, char **argv);
+void parce_map(t_data *map);
 void ft_printf_error(char *str);
 void print_map(char **map);
+void	load_textures(t_data *game);
 
 
 
